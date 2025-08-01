e12a3bc00864db533fa6e1ce05e5eb7f896070fe
gyro1515 <gyro1515@naver.com>
2025-08-01 18:08:09 +0900

    [Feat] 보스가 대시 도중 벽에 충돌하면 벽을 타며 추가적으로 대시하기

--- Assets/Main/Scripts/Character/Boss/Bahamut.cs
+++ Assets/Main/Scripts/Character/Boss/Bahamut.cs
@@ -1,46 +1,135 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EventSystems;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class Bahamut : Enemy
 {
+    Vector2 prePlayerPos = Vector2.zero;
+    [Header("Bahamut 세팅")]
+    [SerializeField] float dashMultiple = 3.0f;
+    [SerializeField] float bodyDamage = 1.0f;
+    [SerializeField] float bodyDamageTime = 0.1f;
+    bool bPlayerInBody = false;
+    float bodyDamageTimer = 0.3f; 
+
+    protected CapsuleCollider2D bodyCol; // 기존 콜라이더
+    protected CapsuleCollider2D trigerCol; // 몸박용 트리거 콜라이더 설정
+    
     protected override void Awake()
     {
         base.Awake();
         behaviorTreeRoot = SetBehaviorTree();
+        bodyCol = GetComponent<CapsuleCollider2D>();
+        // 몸 박용, 같은 크기의 캡슐 콜라이더 생성, 플레이어만 충돌판정 나도록
+        trigerCol = gameObject.AddComponent<CapsuleCollider2D>();
+        trigerCol.isTrigger = true;
+        trigerCol.size = bodyCol.size;
+        int include = LayerMask.GetMask("Player");
+        trigerCol.includeLayers = include; // 포함
+        int exclude = 0;
+        exclude |= 1 << 0; 
+        exclude |= 1 << 1;
+        exclude |= 1 << 2;
+        exclude |= 1 << 4;
+        exclude |= 1 << 5;
+        exclude |= 1 << 6;
+        exclude |= 1 << 8;
+        trigerCol.excludeLayers = exclude; // 제외
+
+        bodyDamageTimer = bodyDamageTime; // 몸박당하자 마자 데미지 주기
     }
+    protected override void Update()
+    {
+        base .Update();
+        if (!bPlayerInBody) return;
+        AddBodyDamageToTarget();
+        
+    }
+    // 우선 바하뮤트만 충돌 설정
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 벽에 부딪혔을 때 법선 구하기
+        Vector2 wallNormal = collision.contacts[0].normal;
+        // 만약 법선과 moveDirection의 방향이 180도에 가깝다면 대시 중지
+        float angle = Vector2.Angle(wallNormal, moveDirection);
+        if (angle > 170f)
+        {
+            prePlayerPos = transform.position;
+            return;
+        }
+
+        // 기존 대시 벡터에서 수직 성분 제거 → 벽을 따라 미끄러지는 방향 계산
+        Vector2 slideDirection = moveDirection - Vector2.Dot(moveDirection, wallNormal) * wallNormal;
+        // 바하뮤트의 이동방향 갱신
+        moveDirection = slideDirection.normalized;
+
+        // 남은 거리 구하기
+        float dist = Vector2.Distance(transform.position, prePlayerPos);
+        // prePlayerPos 갱신
+        prePlayerPos = (Vector2)transform.position + moveDirection * dist;
+    }
+    /*private void OnCollisionStay2D(Collision2D collision)
+    {
+        //collision.gameObject.layer
+        Debug.Log($"Collision 물리 충돌: {collision.gameObject.name}");
+        //Physics2D.CapsuleCast()
+        //prePlayerPos = transform.position;
+    }*/
     
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"Trigger 몸박시작: {collision.gameObject.name}");
+        bPlayerInBody = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log($"Trigger 몸박종료: {collision.gameObject.name}");
+        bPlayerInBody = false;
+    }
+
     protected override INode SetBehaviorTree()
     {
-        ActionNode isActive = new ActionNode(IsActive); // 액션 노드는 함수 바인딩
-        ActionNode moveToCenter = new ActionNode(MoveToCenter);
-        ActionNode canAttack = new ActionNode(CanAttack); // 액션 노드는 함수 바인딩
-        // 아래는 노션의 패턴 순서랑 다릅니다. 비헤이비어 트리 특성상 첫번째로 체크하는 패턴이 가장 강해야 할겁니다.
-        // -> 패턴 사용 가능 여부 체크를 1 -> 2 -> 3 -> 4 순으로 하기 때문에 1, 2, 3, 4 쿨타임이 다 돌아와도 1부터 사용합니다.
-        // 패턴 1: 아마 가장 강한 스킬을 넣어야하지 않을까 싶습니다.
-        ActionNode attack1 = new ActionNode(Attack1); // 액션 노드는 함수 바인딩
-        WaitNode attack1Time = new WaitNode(3.0f); // 공격 지속 시간에 해당, 공격하고 일정 시간 비헤이비어 트리 상태 유지
+        // 액션 노드는 함수 바인딩
+
+        // activeSequence 내용
+        // 활성화되었다면 patternSequence체크, 비활성이고 보스가 맵 중앙이 아니라면 보스가 중앙으로 이동 후 PowerOff
+        ActionNode isActive = new ActionNode(IsActive);  // 활성화 여부 체크
+        ActionNode moveToCenter = new ActionNode(MoveToCenter); // Deactivate라면 보스 위치 초기화
+        SequenceNode activeSequence = new SequenceNode(new List<INode>() { isActive, moveToCenter });
+
+        // patternSequence 내용
+        ActionNode canAttack = new ActionNode(CanAttack); // 공격 범위 && 패턴 사용 가능 체크
+        // 패턴 1:
+        ActionNode attack1 = new ActionNode(Attack1);
+        WaitNode attack1Time = new WaitNode(3.5f); // 공격 지속 시간에 해당, 공격하고 일정 시간 비헤이비어 트리 상태 유지
         SequenceNode attack1Sequence = new SequenceNode(new List<INode>() { attack1, attack1Time }); // 공격이 끝나야 성공처리
         // 패턴 2
         ActionNode attack2 = new ActionNode(Attack2); 
-        WaitNode attack2Time = new WaitNode(2.0f); 
+        WaitNode attack2Time = new WaitNode(3.0f); 
         SequenceNode attack2Sequence = new SequenceNode(new List<INode>() { attack2, attack2Time });
         // 패턴 3
         ActionNode attack3 = new ActionNode(Attack3); 
-        WaitNode attack3Time = new WaitNode(2.0f); 
+        WaitNode attack3Time = new WaitNode(2.5f); 
         SequenceNode attack3Sequence = new SequenceNode(new List<INode>() { attack3, attack3Time }); 
         // 패턴 4
         ActionNode attack4 = new ActionNode(Attack4); 
-        WaitNode attack4Time = new WaitNode(2.0f); 
+        WaitNode attack4Time = new WaitNode(1.5f); 
         SequenceNode attack4Sequence = new SequenceNode(new List<INode>() { attack4, attack4Time });
-
         // 패턴 셀렉터, 패턴 중 사용 가능한 패턴부터 사용.
         SelectorNode patternSelector = new SelectorNode(new List<INode>() { attack1Sequence, attack2Sequence, attack3Sequence, attack4Sequence });
-        ActionNode chase = new ActionNode(Chase);
-        SequenceNode attackSequence = new SequenceNode(new List<INode>() { canAttack, patternSelector });
-        SequenceNode activeSequence = new SequenceNode(new List<INode>() { isActive, moveToCenter });
-        SelectorNode selector = new SelectorNode(new List<INode>() { activeSequence, attackSequence, chase }); // 루트에 해당
+        // 패턴 시전 가능 체크하고, 사용 가능한 패턴들 체크(패턴 쿨타임 체크)
+        SequenceNode patternSequence = new SequenceNode(new List<INode>() { canAttack, patternSelector });
+
+        // ChaseSel 내용
+        ActionNode chase = new ActionNode(Chase); // 일정 범위 안이라면 느릿하게 플레이어 쫓기
+        WaitNode chaseTime = new WaitNode(0.5f); // 일정 시간마다 보스의 움직임 방향 바꾸기
+        SequenceNode chaseSeq = new SequenceNode(new List<INode>() { chase, chaseTime }); // 방향 정하고, 
+        ActionNode dashToPlayerPos = new ActionNode(DaseToPlayerPos);
+        SelectorNode chaeSel = new SelectorNode(new List<INode>() { chaseSeq, dashToPlayerPos });
+
+        SelectorNode selector = new SelectorNode(new List<INode>() { activeSequence, patternSequence, chaeSel }); // 루트에 해당
 
         return selector;
     }
@@ -53,12 +142,7 @@ public class Bahamut : Enemy
     INode.ENodeState Chase()
     {
         //Debug.Log("Chase");
-        if (Vector3.Distance(target.transform.position, transform.position) <= attackRange) // 공격 범위라면 가만히 있기
-        {
-            moveDirection = Vector2.zero;
-            return INode.ENodeState.Success;
-        }
-        else if (Vector3.Distance(target.transform.position, transform.position) <= chaseRange) // 범위 안이라면 쫓아가기
+       if (Vector3.Distance(target.transform.position, transform.position) <= chaseRange) // 범위 안이라면 쫓아가기
         {
             //Debug.Log("ChaseSuccess");
             moveDirection = (target.transform.position - transform.position).normalized;
@@ -71,39 +155,57 @@ public class Bahamut : Enemy
 
             return INode.ENodeState.Success;
         }
-        else if (Vector3.Distance(target.transform.position, transform.position) <= chaseRange * 2.0f) // 범위 * 2 안이라면 타겟 스피드의 2배로 쫓아가기
+        else // 아니라면 대시
         {
-            //Debug.Log("ChaseSuccess");
+            //Debug.Log("ChaseFailureAndDash");
             moveDirection = (target.transform.position - transform.position).normalized;
-            MoveSpeed = target.GetComponent<Player>().MoveSpeed * 2; // 타겟 스피드의 2배로
+            // 변수 따로 빼기?**************
+            MoveSpeed = target.GetComponent<Player>().MoveSpeed * dashMultiple; // 타겟 스피드의 n배로
+            //bodyCol.isTrigger = true;
+            //bodyCol.enabled = false;
+            prePlayerPos = target.transform.position;
+            return INode.ENodeState.Failure; // 이 다음에 행동 노드가 있다면 다른 걸 해야할 수도 있다. 지금은 상관 없다.
+        }
 
-            // 회전 하면서 쫓아가기
-            /*Vector3 dir = target.transform.position - transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 180f;*/
-            //transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    INode.ENodeState DaseToPlayerPos()
+    {
+        /*// 대쉬 끝지점에 도착했다면 리턴 성공
+        if ((Vector2)transform.position == prePlayerPos)
+        {
+            MoveSpeed = baseSpeed;
+            moveDirection = Vector2.zero;
+            return INode.ENodeState.Success;
+        }*/
+
+        if (Vector3.Distance(prePlayerPos, transform.position) < 0.5f) // 거리가 일정 거리 미만이라면 위치 갱신 후 리턴 성공
+        {
+            transform.position = prePlayerPos;
+            MoveSpeed = baseSpeed;
+            moveDirection = Vector2.zero;
+            //bodyCol.isTrigger = false;
+            //bodyCol.enabled = true;
+
 
             return INode.ENodeState.Success;
         }
-        else
-        {
-            //Debug.Log("ChaseFailure");
-            moveDirection = Vector2.zero;
-            MoveSpeed = baseSpeed;
-            return INode.ENodeState.Failure; // 이 다음에 행동 노드가 있다면 다른 걸 해야할 수도 있다. 지금은 상관 없다.
-
-        }
-
+        return INode.ENodeState.Running;
     }
     INode.ENodeState CanAttack()
     {
         //Debug.Log("CanAttack");
-
         // 공격 범위 내 혹은 공격 중이라면 성공 판정
         //if (Vector3.Distance(target.transform.position, transform.position) <= attackRange || isAttacking)
-        if (Vector3.Distance(target.transform.position, transform.position) <= attackRange)
+
+
+        // 공격 범위 내이면서 패턴 사용 가능 상태라면 성공판정
+        if (Vector3.Distance(target.transform.position, transform.position) <= attackRange) // && SkillEntryd의 isPatternEnd도 같이 확인
         {
             //Debug.Log("CanAttackSuccess");
             moveDirection = Vector2.zero;
+            Vector2 lookDir = (target.transform.position - transform.position);
+            // 애니메이션 회전, 부모에서 MoveAnimation(moveDirection)을 실행하더라도 moveDirection = Vector2.zero이기때문에 lookDir회전값으로 적용됨
+            MoveAnimation(lookDir);
             return INode.ENodeState.Success;
         }
 
@@ -185,10 +287,21 @@ public class Bahamut : Enemy
             return INode.ENodeState.Failure;
         }
     }
-    INode.ENodeState Attack5() // 만약 랜덤이라면 아래 형식을 따르면 될겁니다.
+    /*INode.ENodeState Attack5() // 만약 랜덤이라면 아래 형식을 따르면 될겁니다.
     {
-        // 랜덤으로 스킬 고르기
-        bool canUseSkill = Random.Range(0, 2) == 0 ? true : false;
+        // 패턴을 사용할 수 있는가
+
+        // 사용할 수 있다면
+        // 쿨타임 돌아온 것들만 선별해서 랜덤으로 스킬 고르기
+        // 만약 쿨타임 돌아온 스킬이 없다면 리턴 실패
+        // 있다면 랜덤 선택 후, 스킬 사용, 리턴 성공
+
+        // 사용하고 리턴 성공
+        // 사용할 수 없다면
+        // 리턴 실패
+
+
+        bool canUseSkill = Random.Range(0, 4) == 0 ? true : false;
         if(canUseSkill) // 사용 가능한 스킬이라면
         {
             Debug.Log("사용 가능한 스킬 사용!!");
@@ -199,9 +312,7 @@ public class Bahamut : Enemy
             Debug.Log("해당 스킬은 쿨타임입니다.");
             return INode.ENodeState.Failure;
         }
-        
-
-    }
+    }*/
     INode.ENodeState IsActive()
     {
         //Debug.Log("IsActive");
@@ -241,5 +352,15 @@ public class Bahamut : Enemy
         //Debug.Log("MoveToCenterSuccess");
         BossState = EBossState.PowerOff;
         return INode.ENodeState.Success;
+    }
+   
+    void AddBodyDamageToTarget()
+    {
+        bodyDamageTimer += Time.deltaTime;
+        if (bodyDamageTimer >= bodyDamageTime)
+        {
+            bodyDamageTimer -= bodyDamageTime;
+            Debug.Log($"플레이어에게 몸박 데미지{bodyDamage}");
+        }
     }
 }
