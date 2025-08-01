620cacab82b7eb682a874a0b7536516259ae065d
gyro1515 <gyro1515@naver.com>
2025-08-01 12:08:00 +0900

    [Fix] 맵 나갈 시 보스 행동 애니메이션 작동하도록 수정

--- Assets/Main/Scripts/Character/Boss/Bahamut.cs
+++ Assets/Main/Scripts/Character/Boss/Bahamut.cs
@@ -20,24 +20,20 @@ public class Bahamut : Enemy
         // -> 패턴 사용 가능 여부 체크를 1 -> 2 -> 3 -> 4 순으로 하기 때문에 1, 2, 3, 4 쿨타임이 다 돌아와도 1부터 사용합니다.
         // 패턴 1: 아마 가장 강한 스킬을 넣어야하지 않을까 싶습니다.
         ActionNode attack1 = new ActionNode(Attack1); // 액션 노드는 함수 바인딩
-        CooldownNode attack1CoolTime = new CooldownNode(attack1, 10.0f); // 액션 노드와 해당 액션 노드의 쿨타임 설정
         WaitNode attack1Time = new WaitNode(3.0f); // 공격 지속 시간에 해당, 공격하고 일정 시간 비헤이비어 트리 상태 유지
-        SequenceNode attack1Sequence = new SequenceNode(new List<INode>() { attack1CoolTime, attack1Time }); // 공격이 끝나야 성공처리
+        SequenceNode attack1Sequence = new SequenceNode(new List<INode>() { attack1, attack1Time }); // 공격이 끝나야 성공처리
         // 패턴 2
         ActionNode attack2 = new ActionNode(Attack2); 
-        CooldownNode attack2CoolTime = new CooldownNode(attack1, 7.0f); 
         WaitNode attack2Time = new WaitNode(2.0f); 
-        SequenceNode attack2Sequence = new SequenceNode(new List<INode>() { attack1CoolTime, attack1Time });
+        SequenceNode attack2Sequence = new SequenceNode(new List<INode>() { attack2, attack2Time });
         // 패턴 3
         ActionNode attack3 = new ActionNode(Attack3); 
-        CooldownNode attack3CoolTime = new CooldownNode(attack1, 5.0f); 
         WaitNode attack3Time = new WaitNode(2.0f); 
-        SequenceNode attack3Sequence = new SequenceNode(new List<INode>() { attack1CoolTime, attack1Time }); 
+        SequenceNode attack3Sequence = new SequenceNode(new List<INode>() { attack3, attack3Time }); 
         // 패턴 4
         ActionNode attack4 = new ActionNode(Attack4); 
-        CooldownNode attack4CoolTime = new CooldownNode(attack1, 3.0f);
         WaitNode attack4Time = new WaitNode(2.0f); 
-        SequenceNode attack4Sequence = new SequenceNode(new List<INode>() { attack1CoolTime, attack1Time }); // 공격이 끝나야 성공처리
+        SequenceNode attack4Sequence = new SequenceNode(new List<INode>() { attack4, attack4Time });
 
         // 패턴 셀렉터, 패턴 중 사용 가능한 패턴부터 사용.
         SelectorNode patternSelector = new SelectorNode(new List<INode>() { attack1Sequence, attack2Sequence, attack3Sequence, attack4Sequence });
@@ -116,7 +112,18 @@ public class Bahamut : Enemy
     INode.ENodeState Attack1()
     {
         // 여기서 스킬(패턴) 사용!
-        Debug.Log("스킬 패턴1 사용!!");
+        // 스킬 쿨타임 여부
+        bool canUseSkill = Random.Range(0, 2) == 0 ? true : false;
+        if (canUseSkill) // 쿨타임이 다 돌았다면
+        {
+            Debug.Log("스킬 패턴1 사용!!");
+            return INode.ENodeState.Success;
+        }
+        else
+        {
+            Debug.Log("스킬 패턴1은 쿨타임입니다.");
+            return INode.ENodeState.Failure;
+        }
 
         /*if (!isAttacking) // 공격 중이 아니라면 
         {
@@ -131,28 +138,52 @@ public class Bahamut : Enemy
             return INode.ENodeState.Running; // 진행 중 표시, 노드가 다른 곳으로 안가도록
         }
         isAttacking = false;*/
-        return INode.ENodeState.Success;
     }
     INode.ENodeState Attack2()
     {
         // 여기서 스킬(패턴) 사용!
-        Debug.Log("스킬 패턴2 사용!!");
-
-        return INode.ENodeState.Success;
+        // 스킬 쿨타임 여부
+        bool canUseSkill = Random.Range(0, 2) == 0 ? true : false;
+        if (canUseSkill) // 쿨타임이 다 돌았다면
+        {
+            Debug.Log("스킬 패턴2 사용!!");
+            return INode.ENodeState.Success;
+        }
+        else
+        {
+            Debug.Log("스킬 패턴2은 쿨타임입니다.");
+            return INode.ENodeState.Failure;
+        }
     }
     INode.ENodeState Attack3()
     {
-        // 여기서 스킬(패턴) 사용!
-        Debug.Log("스킬 패턴3 사용!!");
-
-        return INode.ENodeState.Success;
+        // 스킬 쿨타임 여부
+        bool canUseSkill = Random.Range(0, 2) == 0 ? true : false;
+        if (canUseSkill) // 쿨타임이 다 돌았다면
+        {
+            Debug.Log("스킬 패턴3 사용!!");
+            return INode.ENodeState.Success;
+        }
+        else
+        {
+            Debug.Log("스킬 패턴3은 쿨타임입니다.");
+            return INode.ENodeState.Failure;
+        }
     }
     INode.ENodeState Attack4()
     {
-        // 여기서 스킬(패턴) 사용!
-        Debug.Log("스킬 패턴4 사용!!");
-
-        return INode.ENodeState.Success;
+        // 스킬 쿨타임 여부
+        bool canUseSkill = Random.Range(0, 2) == 0 ? true : false;
+        if (canUseSkill) // 쿨타임이 다 돌았다면
+        {
+            Debug.Log("스킬 패턴4 사용!!");
+            return INode.ENodeState.Success;
+        }
+        else
+        {
+            Debug.Log("스킬 패턴4은 쿨타임입니다.");
+            return INode.ENodeState.Failure;
+        }
     }
     INode.ENodeState Attack5() // 만약 랜덤이라면 아래 형식을 따르면 될겁니다.
     {
@@ -166,13 +197,14 @@ public class Bahamut : Enemy
         else
         {
             Debug.Log("해당 스킬은 쿨타임입니다.");
-            return INode.ENodeState.Success;
+            return INode.ENodeState.Failure;
         }
         
 
     }
     INode.ENodeState IsActive()
     {
+        //Debug.Log("IsActive");
         if (BossState == EBossState.Deactivate)
         {
             return INode.ENodeState.Success;
@@ -186,7 +218,6 @@ public class Bahamut : Enemy
     INode.ENodeState MoveToCenter()
     {
         //Debug.Log("MoveToCenter");
-        moveDirection = Vector2.zero;
         MoveSpeed = baseSpeed;
         if ((Vector2)transform.position != myRoom.Room.center)
         {
@@ -195,15 +226,18 @@ public class Bahamut : Enemy
             // 일정 거리 안이라면 그냥 가운데로 가게끔
             if (tmpDir.magnitude > 0.1f)
             {
-                transform.position += (Vector3)(tmpDir.normalized * MoveSpeed * Time.deltaTime);
+                moveDirection = tmpDir.normalized;
+                //transform.position += (Vector3)(tmpDir.normalized * MoveSpeed * Time.deltaTime);
             }
             else
             {
+                moveDirection = Vector2.zero;
                 transform.position = myRoom.Room.center;
             }
             //return INode.ENodeState.Running; // 러닝으로 하면 돌아가는 도중 맵 입장시 보스 활성화 안됨
             return INode.ENodeState.Success;
         }
+        MoveAnimation(Vector2.down);
         //Debug.Log("MoveToCenterSuccess");
         BossState = EBossState.PowerOff;
         return INode.ENodeState.Success;
