48ede0db0576af0ff2b733749f96a4deb7e4751f
dragonk1379 <dragonk1379@gmail.com>
2025-08-04 23:13:49 +0900

    [Feat] 엔딩시퀀스 추가 데스나이트 크기 수정

--- Assets/Main/Scripts/UI/EndingUI.cs
+++ Assets/Main/Scripts/UI/EndingUI.cs
@@ -6,6 +6,7 @@ public class EndingUI : MonoBehaviour
 {
     [SerializeField] private Image fadeImage;
     [SerializeField] private float scrollSpeed = 50f;
+    [SerializeField] private float playerSpeed = 3f;
     private float lastPositionY = 777f;
 
     private void Start()
@@ -13,6 +14,8 @@ public class EndingUI : MonoBehaviour
         fadeImage.GetComponent<FadeCamera>().FadeOut();
         AudioManager.Instance.StopBGM();
         AudioManager.Instance.PlayBGM(BGMType.Ending);
+        //fadeImage.GetComponentInChildren<GameObject>(true).SetActive(true);
+        fadeImage.transform.Find("Player").gameObject.SetActive(true);
     }
     void Update()
     {
@@ -23,6 +26,7 @@ public class EndingUI : MonoBehaviour
     {
         if (transform.position.y < lastPositionY)
         {
+            // 스탭롤 처리
             Vector2 position = transform.position;
             position.y += scrollSpeed * Time.deltaTime;
             transform.position = position;
@@ -33,6 +37,22 @@ public class EndingUI : MonoBehaviour
             {
                 // TODO 클릭했을때 스타트화면으로 넘어가기
                 Debug.Log("스타트 화면으로 전이");
+            }
+        }
+        // 플레이어 처리
+        if (fadeImage.transform.Find("Player") != null) 
+        {
+            Vector2 playerPosition = fadeImage.transform.Find("Player").transform.position;
+            playerPosition.y -= playerSpeed * Time.deltaTime;
+            if (playerPosition.y < 200f && playerPosition.x > 400f)
+            {
+                playerPosition.x -= playerSpeed * Time.deltaTime * 1.5f;
+            }
+            //Debug.Log($"position : {playerPosition}");
+            fadeImage.transform.Find("Player").transform.position = playerPosition;
+            if (playerPosition.y < -100f)
+            {
+                Destroy(fadeImage.transform.Find("Player").gameObject);
             }
         }
     }
